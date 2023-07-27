b1b0233fa97eed34fa7c779485b59d4987fdbc76
Evan Cendekia <evan.cendekia@gmail.com>
2023-07-28 02:27:12 +0700

    setup some component

---
+++ src/component/nav/Nav.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import './nav.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+
+const Nav = () => {
+  const [activenav, setActiveNav] = useState('#')
+  return (
+    <nav>
+      <a href='#' onClick={()=>setActiveNav('#')}className={activenav == '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={()=>setActiveNav('#about')} className={activenav == '#about' ? 'active' : ''} ><AiOutlineUser/></a>
+      <a href='#experience' onClick={()=>setActiveNav('#experience')} className={activenav == '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href='#services' onClick={()=>setActiveNav('#services')} className={activenav == '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#contact' onClick={()=>setActiveNav('#contact')} className={activenav == '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </nav>
+  )
+}
+
+export default Nav
